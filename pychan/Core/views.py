ebb7e4a5dd824811aec6f737b52c8c9e6ef06aeb
raccoondude <email@raccoondude.com>
2020-05-25 17:54:40 -0400

    owo

--- pychan/Core/views.py
+++ pychan/Core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
+import urllib
+import urllib.request
+import urllib3
+import json
+import requests
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
 # Create your views here.
 
 GOOGLE_RECAPTCIA_REQUIRED = True
@@ -39,7 +47,33 @@ def postingHandler(request, board_ID):
                 OwO.save()
                 return HttpResponseRedirect("/"+board_ID)
         else:
-            return HttpResponse("Add me lol")
+            recaptcha_response = request.POST['g-recaptcha-response']
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            values = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            req = requests.post(url, data=values)
+            result_json = req.json()
+            ''' End reCAPTCHA validation '''
+            print(result_json)
+            if result_json['success']:
+                try:
+                    currentBoard = board.objects.get(board_ID=board_ID)
+                except board.DoesNotExist:
+                    return HttpResponse("Error")
+                Text = request.POST["text"]
+                File = request.FILES["File"]
+                x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+                if x_forwarded_for:
+                    ip = x_forwarded_for.split(',')[0]
+                else:
+                    ip = request.META.get('REMOTE_ADDR')
+                    OwO = Post(Post_Text=Text, Post_IP=ip, Post_Photo=File, Post_Board=currentBoard)
+                    OwO.save()
+                    return HttpResponseRedirect("/"+board_ID)
+            else:
+                return HttpResponse("Error")
     else:
         try:
             currentBoard = board.objects.get(board_ID=board_ID)
