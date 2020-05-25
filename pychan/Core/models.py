f2fcc6b10011b3c14d88fa94398f40968231a75a
raccoondude <email@raccoondude.com>
2020-05-25 04:22:16 -0400

    OwO

--- pychan/Core/models.py
+++ pychan/Core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class board(models.Model):
+    board_ID = models.CharField(max_length=5, primary_key=True)
+    board_name = models.CharField(max_length=20)
+    board_desc = models.TextField(max_length=1000)
+    board_picture = models.ImageField(upload_to="board")
--- pychan/Core/views.py
+++ pychan/Core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def homepage(request):
     return render(request, "homepage.html")
+
+def board(request, board_ID):
+    return HttpResponse(board_ID)
