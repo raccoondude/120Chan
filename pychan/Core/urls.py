185ac8cfcdf73663248f3e7a1ecbb6610f97e0d1
raccoondude <email@raccoondude.com>
2020-05-25 06:08:04 -0400

    OwO

--- pychan/Core/urls.py
+++ pychan/Core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("board/<str:board_ID>", views.boardHandler, name="boardHandler"),
+    path("board/<str:board_ID>/", views.boardHandler, name="boardHandler"),
+    path("board/<str:board_ID>/post/", views.postingHandler, name="postingHandler"),
 ]
--- pychan/Core/views.py
+++ pychan/Core/views.py
@@ -11,9 +11,32 @@ def boardHandler(request, board_ID):
         currentBoard = board.objects.get(board_ID=board_ID)
     except board.DoesNotExist:
         return HttpResponse("404")
-    Posts = Post.objects.filter(Post_Board=currentBoard)
+    Posts = Post.objects.filter(Post_Board=currentBoard).order_by("-Post_ID")
     return render(request, "board.html", {"boardName":currentBoard.board_name,
     "boardDesc":currentBoard.board_desc,
     "boardPic":currentBoard.board_picture,
     "Posts":Posts,
     })
+
+def postingHandler(request, board_ID):
+    if request.method == "POST":
+        try:
+            currentBoard = board.objects.get(board_ID=board_ID)
+        except board.DoesNotExist:
+            return HttpResponse("Error")
+        Text = request.POST["text"]
+        File = request.FILES["File"]
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        OwO = Post(Post_Text=Text, Post_IP=ip, Post_Photo=File, Post_Board=currentBoard)
+        OwO.save()
+        return HttpResponse("lol")
+    else:
+        try:
+            currentBoard = board.objects.get(board_ID=board_ID)
+        except board.DoesNotExist:
+            return HttpResponse("404")
+        return render(request, "posting.html", {"name":currentBoard.board_name})
