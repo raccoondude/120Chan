b7ed07b0d26d89f17f693dec01345f2bded4d276
raccoondude <email@raccoondude.com>
2020-05-25 15:13:59 -0400

    owo

--- pychan/Core/views.py
+++ pychan/Core/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 # Create your views here.
 
+GOOGLE_RECAPTCIA_REQUIRED = True
+
 def homepage(request):
     return render(request, "homepage.html")
 
@@ -21,20 +23,23 @@ def boardHandler(request, board_ID):
 
 def postingHandler(request, board_ID):
     if request.method == "POST":
-        try:
-            currentBoard = board.objects.get(board_ID=board_ID)
-        except board.DoesNotExist:
-            return HttpResponse("Error")
-        Text = request.POST["text"]
-        File = request.FILES["File"]
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
+        if GOOGLE_RECAPTCIA_REQUIRED == False:
+            try:
+                currentBoard = board.objects.get(board_ID=board_ID)
+            except board.DoesNotExist:
+                return HttpResponse("Error")
+            Text = request.POST["text"]
+            File = request.FILES["File"]
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0]
+            else:
+                ip = request.META.get('REMOTE_ADDR')
+                OwO = Post(Post_Text=Text, Post_IP=ip, Post_Photo=File, Post_Board=currentBoard)
+                OwO.save()
+                return HttpResponseRedirect("/"+board_ID)
         else:
-            ip = request.META.get('REMOTE_ADDR')
-        OwO = Post(Post_Text=Text, Post_IP=ip, Post_Photo=File, Post_Board=currentBoard)
-        OwO.save()
-        return HttpResponseRedirect("/"+board_ID)
+            return HttpResponse("Add me lol")
     else:
         try:
             currentBoard = board.objects.get(board_ID=board_ID)
