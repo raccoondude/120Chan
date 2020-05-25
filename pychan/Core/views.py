055210acd162bc5cf4c9e47f731e16eb68120d14
raccoondude <email@raccoondude.com>
2020-05-25 04:30:14 -0400

    owo

--- pychan/Core/views.py
+++ pychan/Core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def homepage(request):
     return render(request, "homepage.html")
 
-def board(request, board_ID):
-    return HttpResponse(board_ID)
+def boardHandler(request, board_ID):
+    try:
+        currentBoard = board.objects.get(board_ID=board_ID)
+    except board.DoesNotExist:
+        return HttpResponse("404")
+    return render(request, "board.html")
